9c56772d8a572bf79d83e5ae2c9ec37eaeb2d34c
Samar Seth <samarseth@aol.com>
2018-07-26 13:29:08 -0700

    Added validators

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,9 @@
 class Course < ApplicationRecord
   has_many :challenges
   has_and_belongs_to_many :students
+
+  validates :name, :students, :challenges, :presence => true
+  validates :name, :challenges, :length => {:minimum => 1}
+  validates :name, :uniquesness => true
+  
 end
