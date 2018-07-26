f37a3cc0869e3aa087b492d7df0218fdc60bc386
Samar Seth <samarseth@aol.com>
2018-07-26 13:11:07 -0700

    Test

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+
   devise_for :students, :controllers => { :omniauth_callbacks => "students/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'courses#index'
 
 
   resources :students do
-    resources :courses do
-      resources :challenges
+  resources :courses do
+  	get 'add_student_to_course' => 'courses#add_student_to_course'
+  resources :challenges
     end
   end
 end
