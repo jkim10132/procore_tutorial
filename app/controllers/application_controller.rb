595c9c277669866c9bb429965c1c1a199480be7c
Bryan Lim <bryan.lim@procore.com>
2018-07-27 08:38:25 -0700

    Redefine current_user as current_student

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
   before_action :authenticate_student!
+
+  def pundit_user
+    current_student
+  end
 end
